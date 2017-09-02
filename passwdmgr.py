780d3f51e4db0977ce66c918f5651b5cc229bd92
apt-helion <aphelion030@gmail.com>
2017-09-02 19:54:15 +1000

    Done all important stuff

--- passwdmgr.py
+++ passwdmgr.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
+import random
+import pwd
 import sys
 import os
-import pwd
 import re
 
 USER = pwd.getpwuid(os.getuid())[0]
-PATH = "/home/" + USER + "/passwdmgr/"
+PATH = "/home/{}/passwdmgr/".format(USER)
 PATTERN = r'(\/)+'
 
 def new():
-    reg = re.split(PATTERN, sys.argv[2])
+    try:
+        reg = re.split(PATTERN, sys.argv[2])
+    except IndexError:
+        print("ERROR: File not specified\nType 'passwdmgr help' for help")
+        return 0
     path_ = "".join(reg[:-1])
     file_ = reg[-1]
 
@@ -23,7 +28,6 @@ def new():
     else:
         print("ERROR: File '%s' already exists.\n" % (file_) + \
               "Type 'passwdmgr help' for help.")
-        return 0
 
 def insert():
     try:
@@ -33,6 +37,9 @@ def insert():
         return 0
     except FileNotFoundError:
         print("ERROR: '%s' not found." % (sys.argv[2]))
+        return 0
+    except IndexError:
+        print("ERROR: File not specified\nType 'passwdmgr help' for help")
         return 0
 
     try:
@@ -44,21 +51,50 @@ def insert():
               "Command is 'passwdmgr insert <path> <password>'.")
         return 0
 
+def generate():
+    chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwYyXxZz!@#$%&"
+    password = ""
+    for i in range(0,16):
+        password += chars[random.randint(0,57)]
+
+    try:
+        file_ = open(PATH + sys.argv[2], "r+")
+    except IsADirectoryError:
+        print("ERROR: '%s' is a directory, not file." % (sys.argv[2]))
+        return 0
+    except FileNotFoundError:
+        print("ERROR: '%s' not found." % (sys.argv[2]))
+        return 0
+    except IndexError:
+        print("ERROR: File not specified\nType 'passwdmgr help' for help")
+        return 0
+
+    file_.truncate()
+    file_.write(password)
+    print("The generated password to '%s' is: \n%s" % (sys.argv[2], password))
+
 def remove():
-    reg = re.split(PATTERN, sys.argv[2])
+    try:
+        reg = re.split(PATTERN, sys.argv[2])
+    except IndexError:
+        print("ERROR: File not specified\nType 'passwdmgr help' for help")
+        return 0
+
     path_ = "".join(reg[:-1])
 
     try:
         os.remove(PATH + sys.argv[2])
+    except IsADirectoryError:
+        os.rmdir(PATH + sys.argv[2])
     except FileNotFoundError:
         print("ERROR: No such file: '%s'." % (sys.argv[2]))
         return 0
 
-    # Ask to remove the directory if it's empty
+    # Ask to remove the parent directory if it's empty
     if not os.listdir(PATH + path_):
         userinput = input("The '%s' directory is empty" % (path_) + \
                           "  - Do you want to remove it? (yes/no)\n> ")
-        if userinput.lower() is "yes" or "y":
+        if userinput.lower() is "yes" or userinput.lower() is "y":
             os.rmdir(PATH + path_)
             print("Removed empty directory '%s'." % (path_))
 
@@ -71,9 +107,63 @@ def print_passwd():
     except FileNotFoundError:
         print("ERROR: '%s' not found." % (sys.argv[2]))
         return 0
+    except IndexError:
+        print("ERROR: File not specified\nType 'passwdmgr help' for help")
+        return 0
 
     password = "".join(file_.readlines())
     print(password)
+
+def print_paths():
+    for root, dirs, files in os.walk(PATH):
+        level = root.replace(PATH, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print("%s%s/" % (indent, os.path.basename(root)))
+        subindent = ((' ' * 4) + '|')  * (level + 1)
+        for f in files:
+            print("%s%s" % (subindent, f))
+
+def search():
+    pattern = r'{}'.format(sys.argv[2])
+    for root, dirs, files in os.walk(PATH):
+        level = root.replace(PATH, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        subindent = ((' ' * 4) + '|') * (level + 1)
+        for f in files:
+            if re.search(pattern, f):
+                print("%s%s/" % (indent, os.path.basename(root)))
+                print("%s%s" % (subindent, f))
+
+def show_help():
+    print("""
++++++++++++++++++++++++++++++++++++
+|  passwdmgr: a password manager  |
+|              v1.0               |
+| github.com/apt-helion/passwdmgr |
++++++++++++++++++++++++++++++++++++
+
+Usage:
+    passwdmgr <new/add> [directory/file]
+        Creates a new password file.
+        e.g 'passwdmgr new Email/gmail'
+    passwdmgr <insert> [directory/file] [password]
+        Adds a password to a file.
+        e.g 'passwdmgr insert Business/site.com goodpassword'
+    passwdmgr <generate> [directory/file]
+        Generates a random 16 character password into file
+        e.g 'passwdmgr generate Email/hotmail.com'
+    passwdmgr <remove/rm>
+        Removes a file or directory.
+        e.g 'passwdmgr rm Internet/reddit'
+    passwdmgr <print> [directory/file]
+        Prints a password from the file.
+        e.g 'passwdmgr print Email/yahoo'
+    passwdmgr <ls>
+        Lists all files and folders.
+    passwdmgr <search/find> [filename]
+        Lists all files that match filename with regex.
+        e.g 'passwdmgr search .com'
+""")
 
 def main():
     if len(sys.argv) == 1:
@@ -86,14 +176,20 @@ def main():
             print_paths()
             return 1
 
-    if sys.argv[1] == "new":
+    if sys.argv[1] == "new" or sys.argv[1] == "add":
         new()
     elif sys.argv[1] == "insert":
         insert()
-    elif sys.argv[1] == "remove":
+    elif sys.argv[1] == "generate":
+        generate()
+    elif sys.argv[1] == "remove" or sys.argv[1] == "rm":
         remove()
     elif sys.argv[1] == "print":
         print_passwd()
+    elif sys.argv[1] == "ls":
+        print_paths()
+    elif sys.argv[1] == "search" or sys.argv[1] == "find":
+        search()
     elif sys.argv[1] == "help":
         show_help()
     else:
